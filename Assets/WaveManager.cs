217ccfd0a3414a00f989a2b7056c0ce20876a9b7
Marvin <marvin.k.scharding@gmail.com>
2020-01-25 20:54:29 +0100

    First try of a WaveManager

---
+++ Assets/WaveManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+   [SerializeField] int ennemiToSpawn = 0;
+    int ennemiNumber;
+    int currentWave = 0;
+    [SerializeField] float waveTimer = 0;
+    float waveTime = 0;
+    [SerializeField] GameObject prefabEnnemi;
+    [SerializeField] Transform ennemiSpawnPoint;
+    bool allDead = false;
+    void Start()
+    {
+    }
+    enum State
+    {
+        ORIGINAL_WAVE,
+        PREPARING_NEXT_WAVE,
+        SPAWN_ENNEMIS,
+        CHECK_ENNEMIS_DEATH
+
+
+    }
+    State state = State.ORIGINAL_WAVE;
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state)
+        {
+            case State.ORIGINAL_WAVE:
+                currentWave = 1;
+                ennemiToSpawn = 1;
+                waveTime += waveTimer;
+                state = State.SPAWN_ENNEMIS;
+                break;
+            case State.PREPARING_NEXT_WAVE:
+                currentWave += 1;
+                ennemiToSpawn++;
+                waveTime += waveTimer;
+                allDead = false;
+                state = State.SPAWN_ENNEMIS;
+                break;
+            case State.SPAWN_ENNEMIS:
+               // Debug.Log(ennemiToSpawn);
+                for (int i = 0; i < ennemiToSpawn; i++)
+                {
+                    GameObject ennemi = Instantiate(prefabEnnemi, ennemiSpawnPoint);
+                    ennemiNumber++;
+                }
+                state = State.CHECK_ENNEMIS_DEATH;
+                break;
+            case State.CHECK_ENNEMIS_DEATH:
+               // Debug.Log(ennemiNumber);
+                if (allDead)
+                {
+                    waveTime -= Time.deltaTime;
+                    if (waveTime <= 0)
+                    {
+                        state = State.PREPARING_NEXT_WAVE;
+                    }
+                }
+                break;
+
+        }
+    }
+    public void ennemiDeath()
+    {
+        ennemiNumber--;
+        if(ennemiNumber<=0)
+        {
+            allDead = true;
+        }
+    }
+}
---
+++ Assets/ennemi.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ennemi : MonoBehaviour
+{
+    [SerializeField]float timer;
+    SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+    enum State
+    {
+        ALIVE,
+        DEAD
+    }
+    State state = State.ALIVE;
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state)
+        {
+            case State.ALIVE:
+                timer -= Time.deltaTime;
+                // Debug.Log(timer);
+                if (timer <= 0)
+                {
+                    WaveManager waveManager = FindObjectOfType<WaveManager>();
+                    waveManager.ennemiDeath();
+                    state = State.DEAD;
+                }
+                break;
+            case State.DEAD:
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
